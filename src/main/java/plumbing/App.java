20c95ea51824c6ca6b8cbc1f27ad0a1406011db7
Pete Hodgson <git@thepete.net>
2017-02-04 12:48:13 -0800

    add CORS support

--- src/main/java/plumbing/App.java
+++ src/main/java/plumbing/App.java
@@ -25,6 +25,8 @@ public class App {
 
         redirect.get("/", "/todos");
 
+        addCORS();
+
         post("/todos", "application/json", (request,response) -> {
             NewTodo newTodo = gson.fromJson(request.body(), NewTodo.class);
             Todo todo = controller.createTodo(newTodo);
@@ -33,5 +35,26 @@ public class App {
         }, jsonTransformer);
 
         get("/todos", "application/json", (request, response) -> controller.allTodos(), jsonTransformer);
+    }
+
+    // based on https://sparktutorials.github.io/2016/05/01/cors.html
+    static void addCORS(){
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+        });
+
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
     }
 }
